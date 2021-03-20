921a75ccda295c51511229687a70118a66805d54
dharanishkc <dharanishkc.19cse@kongu.edu>
2021-03-20 19:45:09 +0530

    Added comments to division.py

--- division.py
+++ division.py
@@ -1,7 +1,7 @@
-def doDivision(a,b);
-     return (a/b)
+def dodivision(a,b);
+     return (a/b) # Return the quotient of a and b
 
  a = int(input("Enter a"))
  b = int(input("Enter b"))
-res=doDivision(a,b)
+res=dodivision(a,b)
 print(res)
